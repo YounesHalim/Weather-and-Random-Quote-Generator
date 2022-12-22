e12e023d6bac7f8e15be0c9e8c255f05c266951c
Younes Halim <younes.halim@gmail.com>
2022-12-22 13:59:07 -0500

    Singleton pattern to create a connection

---
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/model/apicall/CreateConnection.java
@@ -0,0 +1,29 @@
+package com.weatherreport.weatherreport.model.apicall;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Singleton pattern
+ */
+public class CreateConnection {
+    private static CreateConnection connectionInstance;
+    private static HttpURLConnection httpURLConnection;
+    private CreateConnection() {}
+    public static synchronized CreateConnection getConnectionInstance() {
+        if(connectionInstance == null) {
+           connectionInstance = new CreateConnection();
+        }
+        return connectionInstance;
+    }
+    public HttpURLConnection getApiConnection(URL url) {
+        try {
+            if(this.httpURLConnection == null) httpURLConnection = (HttpURLConnection) url.openConnection();
+        }catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return httpURLConnection;
+    }
+}
