a14719a7b9b90b5fb1e215db17feac1cfefb3743
Younes Halim <younes.halim@gmail.com>
2022-12-23 16:45:28 -0500

    Opening Gson to image data object

--- WeatherReport/src/main/java/module-info.java
+++ WeatherReport/src/main/java/module-info.java
@@ -15,4 +15,5 @@ module com.weatherreport.weatherreport {
     exports com.weatherreport.weatherreport.controllers;
     opens com.weatherreport.weatherreport.controllers to javafx.fxml;
     opens com.weatherreport.weatherreport.model.meteorology to com.google.gson;
+    opens com.weatherreport.weatherreport.model.images to com.google.gson;
 }
