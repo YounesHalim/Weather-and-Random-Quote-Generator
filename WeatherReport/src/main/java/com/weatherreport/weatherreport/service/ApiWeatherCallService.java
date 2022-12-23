cd88bc34fdad06dd7c88fc87765e840bdb0532e2
Younes Halim <younes.halim@gmail.com>
2022-12-22 22:39:42 -0500

    Singleton

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/service/ApiWeatherCallService.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/service/ApiWeatherCallService.java
@@ -5,23 +5,25 @@ import com.weatherreport.weatherreport.model.apicall.CallOpenWeatherAPI;
 import com.weatherreport.weatherreport.model.location.GeographicLocation;
 import com.weatherreport.weatherreport.model.meteorology.*;
 
-import java.util.List;
-
 public class ApiWeatherCallService implements CallOpenWeatherAPI {
-    private static Meteorology meteorology;
+    private static ApiWeatherCallService apiWeatherCallService;
+    private ApiWeatherCallService() {}
+    public static ApiWeatherCallService getApiWeatherCallServiceInstance() {
+        if(apiWeatherCallService == null) {
+            apiWeatherCallService = new ApiWeatherCallService();
+        }
+        return apiWeatherCallService;
+    }
     @Override
     public String getJSONAsAString(GeographicLocation location) {
         return CallOpenWeatherAPI.super.getJSONAsAString(location);
     }
-
     @Override
     public Meteorology getMeteorologyObject(GeographicLocation cityLocation) {
+        Meteorology meteorology ;
         Gson gson = new Gson();
         meteorology = gson.fromJson(getJSONAsAString(cityLocation), Meteorology.class);
         return meteorology;
     }
-    public static Meteorology getMeteorology() {
-        if(meteorology == null) return null;
-        return meteorology;
-    }
+
 }
