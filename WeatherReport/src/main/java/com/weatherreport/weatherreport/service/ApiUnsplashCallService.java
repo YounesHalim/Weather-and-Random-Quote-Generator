8de00e9bdd33078c9fea60d23d96da2ffeedd1de
Younes Halim <younes.halim@gmail.com>
2022-12-23 16:41:21 -0500

    Unsplash service

---
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/service/ApiUnsplashCallService.java
@@ -0,0 +1,24 @@
+package com.weatherreport.weatherreport.service;
+
+import com.google.gson.Gson;
+import com.weatherreport.weatherreport.model.apicall.CallUnsplashAPI;
+import com.weatherreport.weatherreport.model.images.ImageDataObject;
+import com.weatherreport.weatherreport.model.location.GeographicLocation;
+
+public class ApiUnsplashCallService implements CallUnsplashAPI {
+    private static ApiUnsplashCallService apiUnsplashCallService;
+    private ApiUnsplashCallService() {}
+
+    public static ApiUnsplashCallService unsplashCallServiceInstance() {
+        if(apiUnsplashCallService == null) {
+            apiUnsplashCallService = new ApiUnsplashCallService();
+        }
+        return apiUnsplashCallService;
+    }
+
+    @Override
+    public ImageDataObject deserializeUnsplashJsonObject(GeographicLocation location) {
+        Gson gson = new Gson();
+        return gson.fromJson(CallUnsplashAPI.super.getJSONAsAString(location), ImageDataObject.class);
+    }
+}
