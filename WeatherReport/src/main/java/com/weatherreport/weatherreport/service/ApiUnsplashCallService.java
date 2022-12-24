5cf12ccdef593a2bbf010c2e4ba2ea36ebe0ac50
Younes Halim <younes.halim@gmail.com>
2022-12-23 23:48:30 -0500

    New method

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/service/ApiUnsplashCallService.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/service/ApiUnsplashCallService.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.weatherreport.weatherreport.model.apicall.CallUnsplashAPI;
 import com.weatherreport.weatherreport.model.images.ImageDataObject;
 import com.weatherreport.weatherreport.model.location.GeographicLocation;
+import javafx.scene.image.Image;
+import java.net.URL;
 
 public class ApiUnsplashCallService implements CallUnsplashAPI {
     private static ApiUnsplashCallService apiUnsplashCallService;
@@ -20,5 +22,10 @@ public class ApiUnsplashCallService implements CallUnsplashAPI {
     public ImageDataObject deserializeUnsplashJsonObject(GeographicLocation location) {
         Gson gson = new Gson();
         return gson.fromJson(CallUnsplashAPI.super.getJSONAsAString(location), ImageDataObject.class);
+    }
+
+    @Override
+    public Image getRandomImageBasedOnLocation(URL url) {
+        return null;
     }
 }
