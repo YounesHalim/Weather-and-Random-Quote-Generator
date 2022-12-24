35e8df6ba2295b2fc3c52c8f5c4b18cebc141cb7
Younes Halim <younes.halim@gmail.com>
2022-12-23 23:52:14 -0500

    Minor fix

--- WeatherReport/src/main/java/module-info.java
+++ WeatherReport/src/main/java/module-info.java
@@ -13,7 +13,9 @@ module com.weatherreport.weatherreport {
     opens com.weatherreport.weatherreport to javafx.fxml;
     exports com.weatherreport.weatherreport;
     exports com.weatherreport.weatherreport.controllers;
+    exports com.weatherreport.weatherreport.model.location;
     opens com.weatherreport.weatherreport.controllers to javafx.fxml;
     opens com.weatherreport.weatherreport.model.meteorology to com.google.gson;
     opens com.weatherreport.weatherreport.model.images to com.google.gson;
+    opens com.weatherreport.weatherreport.model.location to com.google.gson;
 }
