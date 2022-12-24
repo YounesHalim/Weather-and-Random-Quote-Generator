1de548ba1f04d669c587724f982578a99af32154
Younes Halim <younes.halim@gmail.com>
2022-12-23 23:49:17 -0500

    Code optimization

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/model/location/GeographicLocation.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/model/location/GeographicLocation.java
@@ -5,10 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class GeographicLocation {
-    private String country, city, measureUnits;
+    private String country, name, lat, lng, measureUnits;
+    private List<GeographicLocation> data;
 }
