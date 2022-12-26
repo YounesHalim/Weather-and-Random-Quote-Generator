65f0d38e402fa9034b7947d1f8ab51f5f7ee3fbd
Younes Halim <younes.halim@gmail.com>
2022-12-26 00:31:25 -0500

    Major update

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/WeatherReportController.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/WeatherReportController.java
@@ -1,6 +1,5 @@
 package com.weatherreport.weatherreport.controllers;
 
-import com.weatherreport.weatherreport.WeatherReportApplication;
 import com.weatherreport.weatherreport.model.location.GeographicLocation;
 import com.weatherreport.weatherreport.model.meteorology.MainWeather;
 import com.weatherreport.weatherreport.model.meteorology.Meteorology;
@@ -8,7 +7,6 @@ import com.weatherreport.weatherreport.model.meteorology.Sys;
 import com.weatherreport.weatherreport.model.meteorology.Weather;
 import com.weatherreport.weatherreport.service.ApiWeatherCallService;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -16,30 +14,29 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
-public class WeatherReportController implements Initializable{
-    @FXML private Label date, feelsLike, sunsetHour, degree, countryLocation;
-    @FXML
-    private Circle weatherIcon;
-    @FXML
-    private BorderPane mainApplicationLayout;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeatherReportController implements Initializable {
+    @FXML public AnchorPane rightAnchorPane;
+    @FXML private Label date, feelsLike, sunsetHour,sunriseHour, degree, countryLocation, weatherDescription, mainDescription;
+    @FXML private Circle weatherIcon;
+    @FXML private BorderPane mainApplicationLayout;
     public static GeographicLocation defaultLocation = GeographicLocation.builder().country("CA").name("Montreal").build();
-    private void execution(GeographicLocation geographicLocation) {
-        Meteorology forecast = setWeatherApiCall(geographicLocation);
-        setWeatherIcon(forecast);
-        long sunset = forecast.getSys().getSunset();
-        setHeliosTime("LAST", sunset);
-        setCountryLocation(forecast, forecast.getSys());
-        setFeelsLike(forecast.getMain());
-        setTemperature(forecast.getMain());
-        setDate();
-    }
+    private String celsius = "%d\u00B0C";
     private void setWeatherIcon(Meteorology forecast) {
         String icon = new Weather().getWeatherAttributes(forecast, Weather::getIcon);
         try {
@@ -49,46 +46,67 @@ public class WeatherReportController implements Initializable{
             throw new RuntimeException(e);
         }
     }
+
     private void setHeliosTime(String firstOrLastLight, long sunriseSunsetTime) {
         Date convertedDate = new Date(sunriseSunsetTime * 1000);
         switch (firstOrLastLight) {
             case "LAST" ->
                     sunsetHour.setText(sunsetHour.getText() + " " + convertedDate.getHours() + ":" + convertedDate.getMinutes());
+            case "FIRST"->
+                    sunriseHour.setText(sunriseHour.getText() + " " + convertedDate.getHours() + ":" + convertedDate.getMinutes());
         }
     }
+
     private void setCountryLocation(Meteorology forecast, Sys country) {
-        countryLocation.setText("%s, %s".formatted(forecast.getName(), country.getCountry()));
+        String s = "%s, %s".formatted(forecast.getName(), country.getCountry());
+        countryLocation.setText(s);
     }
+
     private void setFeelsLike(MainWeather mainWeather) {
         int feelsLikeTemp = (int) (Math.floor(mainWeather.getFeels_like()));
-        feelsLike.setText("%s %d".formatted(feelsLike.getText(), feelsLikeTemp));
+        feelsLike.setText(String.format(celsius,feelsLikeTemp));
     }
+
     private void setTemperature(MainWeather mainWeather) {
-        degree.setText(String.valueOf((int) Math.floor(mainWeather.getTemp())));
+        degree.setText(String.format(celsius,(int) Math.floor(mainWeather.getTemp())));
     }
+
     private void setDate() {
-        date.setText(new Date().toString());
+        date.setText(LocalDate.now().toString());
     }
+
+    private void setDescription(String description) {
+        // Capitalize
+        mainDescription.setText(Arrays.stream(description.split(" "))
+                .map((word)-> word.toUpperCase().charAt(0) + word.substring(1))
+                .collect(Collectors.joining("\n")));
+    }
+
+    private void setMainDescription(String description)  {
+        weatherDescription.setText(description);
+    }
+
     protected Meteorology setWeatherApiCall(GeographicLocation geographicLocation) {
         return ApiWeatherCallService.getApiWeatherCallServiceInstance().weatherApiCall(geographicLocation);
     }
+
     public static void setDefaultLocation(GeographicLocation defaultLocation) {
         WeatherReportController.defaultLocation = defaultLocation;
     }
 
-    private void sceneInjector(String FXMLFileName) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(WeatherReportApplication.class.getResource(FXMLFileName+".fxml"));
-            AnchorPane searchBar = fxmlLoader.load();
-            mainApplicationLayout.setTop(searchBar);
-        }catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        execution(defaultLocation);
-        sceneInjector("SearchBar-Scene");
+        Meteorology forecast = setWeatherApiCall(defaultLocation);
+        setWeatherIcon(forecast);
+        long sunset = forecast.getSys().getSunset();
+        long sunrise = forecast.getSys().getSunrise();
+        setHeliosTime("LAST", sunset);
+        setHeliosTime("FIRST",sunrise);
+        setCountryLocation(forecast, forecast.getSys());
+        setFeelsLike(forecast.getMain());
+        setTemperature(forecast.getMain());
+        setDate();
+        setDescription(new Weather().getWeatherAttributes(forecast,Weather::getDescription));
+        setMainDescription(new Weather().getWeatherAttributes(forecast, Weather::getMain));
     }
 }
