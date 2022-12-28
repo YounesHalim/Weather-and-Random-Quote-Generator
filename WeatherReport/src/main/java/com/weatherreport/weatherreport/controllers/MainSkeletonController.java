9914a4ec54cee9f03b9b99078691545c88b09b8d
Younes Halim <younes.halim@gmail.com>
2022-12-28 14:53:40 -0500

    Enum to handle interface switches

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/MainSkeletonController.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/MainSkeletonController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,43 +16,80 @@ import java.io.IOException;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MainSkeletonController{
-    @FXML private BorderPane mainApplicationLayout;
+public class MainSkeletonController {
+    @FXML
+    private BorderPane mainApplicationLayout;
+    public enum interfaceLoader {
+        SEARCH_BAR("SearchBar-Scene.fxml"),
+        WEATHER_INTERFACE("todaysOverviewLayout.fxml"),
+        NEWS_INTERFACE("newsScene.fxml"),
+        MENU_NAV("menuNavigator.fxml"),
+        MAIN("MainScene.fxml");
+        private String fxmlFile;
+        interfaceLoader(String fxmlFile) {
+            this.fxmlFile = fxmlFile;
+        }
+        public String getInterface() {
+            return fxmlFile;
+        }
+    }
     public void initialize() {
+        weatherInterfaceLoader();
+        searchBarLoader();
+        menuNavigation();
+    }
+
+    private void weatherInterfaceLoader() {
+        FXMLLoader rightPaneLoader = new FXMLLoader();
+        rightPaneLoader.setLocation(WeatherReportApplication.class.getResource(interfaceLoader.WEATHER_INTERFACE.getInterface()));
+        StackPane weatherLookUp;
         try {
-            weatherInterfaceLoader();
-            searchBarLoader();
-            newsInterfaceLoader();
-            widgetInterfaceLoader();
+            weatherLookUp = rightPaneLoader.load();
+            weatherLookUp.setCache(true);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private void weatherInterfaceLoader() throws IOException {
-        FXMLLoader rightPaneLoader = new FXMLLoader();
-        rightPaneLoader.setLocation(WeatherReportApplication.class.getResource("todaysOverViewLayout.fxml"));
-        AnchorPane weatherLookUp = rightPaneLoader.load();
         mainApplicationLayout.setCenter(weatherLookUp);
     }
 
-    private void searchBarLoader() throws IOException {
+    private void searchBarLoader() {
+
         FXMLLoader topPaneLoader = new FXMLLoader();
-        topPaneLoader.setLocation(WeatherReportApplication.class.getResource("SearchBar-Scene.fxml"));
-        AnchorPane searchBar = topPaneLoader.load();
+        topPaneLoader.setLocation(WeatherReportApplication.class.getResource(interfaceLoader.SEARCH_BAR.getInterface()));
+        AnchorPane searchBar;
+        try {
+            searchBar = topPaneLoader.load();
+            searchBar.setCache(true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         mainApplicationLayout.setTop(searchBar);
     }
 
-    private void newsInterfaceLoader() throws IOException {
+    private void newsInterfaceLoader(){
         FXMLLoader rightPaneLoader = new FXMLLoader();
-        rightPaneLoader.setLocation(WeatherReportApplication.class.getResource("newsScene.fxml"));
-        AnchorPane newsInterface = rightPaneLoader.load();
-        mainApplicationLayout.setRight(newsInterface);
+        rightPaneLoader.setLocation(WeatherReportApplication.class.getResource(interfaceLoader.NEWS_INTERFACE.getInterface()));
+        Pane newsInterface;
+        try {
+            newsInterface = rightPaneLoader.load();
+            newsInterface.setCache(true);
+        }catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        mainApplicationLayout.setCenter(newsInterface);
     }
-    private void widgetInterfaceLoader() throws IOException {
+
+    private void menuNavigation() {
         FXMLLoader leftPaneLoader = new FXMLLoader();
-        leftPaneLoader.setLocation(WeatherReportApplication.class.getResource("citiesWidget.fxml"));
-        AnchorPane widgetInterface = leftPaneLoader.load();
+        leftPaneLoader.setLocation(WeatherReportApplication.class.getResource(interfaceLoader.MENU_NAV.getInterface()));
+        AnchorPane widgetInterface;
+        try {
+            widgetInterface = leftPaneLoader.load();
+            widgetInterface.setCache(true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         mainApplicationLayout.setLeft(widgetInterface);
     }
 }
