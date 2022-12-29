c6210887cd2c3ae12dae8c73d9b3ef40ceba40d9
Younes Halim <younes.halim@gmail.com>
2022-12-29 10:37:50 -0500

    Unused package deleted

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/model/apicall/CallGNewsAPI.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/model/apicall/CallGNewsAPI.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
+
 
 public interface CallGNewsAPI {
     default String getJSONAsAString(News news) {
