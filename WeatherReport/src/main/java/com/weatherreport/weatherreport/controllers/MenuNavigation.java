2e584bcdfea18e91b36f484454db73f7eeb53f59
Younes Halim <younes.halim@gmail.com>
2022-12-29 10:54:45 -0500

    Extracting code to functions

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/MenuNavigation.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/MenuNavigation.java
@@ -1,6 +1,7 @@
 package com.weatherreport.weatherreport.controllers;
 
 import com.weatherreport.weatherreport.WeatherReportApplication;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,55 +20,67 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static com.weatherreport.weatherreport.controllers.MainSkeletonController.*;
+import static com.weatherreport.weatherreport.controllers.MainController.*;
 
-public class MenuNavigation implements Initializable{
+public class MenuNavigation implements Initializable {
     @FXML
     private AnchorPane menuController;
-    @FXML private Pane newsPane;
-    @FXML private Button weather, news;
+
+    @FXML
+    private Button weather, news;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         weather.setCursor(Cursor.HAND);
         news.setCursor(Cursor.HAND);
-        weather.setOnAction(actionEvent -> {
-            FXMLLoader rootLoader = new FXMLLoader(WeatherReportApplication.class.getResource(interfaceLoader.MAIN.getInterface()));
-            FXMLLoader weatherLoader = new FXMLLoader(WeatherReportApplication.class.getResource(interfaceLoader.WEATHER_INTERFACE.getInterface()));
-            try {
-                Parent root = rootLoader.load();
-                weatherLoader.load();
-                MainSkeletonController skeletonController = rootLoader.getController();
-                WeatherReportController reportController = weatherLoader.getController();
-                StackPane weatherStackPane = reportController.weatherStackPane;
-                skeletonController.getMainApplicationLayout().setCenter(weatherStackPane);
-                sceneRefresher(actionEvent, root);
+        weather.setOnAction(actionEvent -> Platform.runLater(() -> {
+            weatherInterfaceLoader(actionEvent);
+        }));
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        news.setOnAction(actionEvent -> Platform.runLater(() -> {
+            newsInterfaceLoader(actionEvent);
 
-        news.setOnAction(actionEvent -> {
-            FXMLLoader rootLoader = new FXMLLoader(WeatherReportApplication.class.getResource(interfaceLoader.MAIN.getInterface()));
-            FXMLLoader newsLoader = new FXMLLoader(WeatherReportApplication.class.getResource(interfaceLoader.NEWS_INTERFACE.getInterface()));
-            try {
-                Parent root = rootLoader.load();
-                newsLoader.load();
-                MainSkeletonController skeletonController = rootLoader.getController();
-                NewsReportsController newsReportsController = newsLoader.getController();
-                Pane newsPane = newsReportsController.newsPane;
-                skeletonController.getMainApplicationLayout().setCenter(newsPane);
-                sceneRefresher(actionEvent, root);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        }));
 
+    }
+
+    private static void newsInterfaceLoader(ActionEvent actionEvent) {
+        FXMLLoader rootLoader = new FXMLLoader(WeatherReportApplication.class.getResource(interfaceLoader.MAIN.getInterface()));
+        FXMLLoader newsLoader = new FXMLLoader(WeatherReportApplication.class.getResource(interfaceLoader.NEWS_INTERFACE.getInterface()));
+
+        try {
+            Parent root = rootLoader.load();
+            newsLoader.load();
+            MainController mainController = rootLoader.getController();
+            NewsReportsController newsReportsController = newsLoader.getController();
+            Pane newsPane = newsReportsController.newsPane;
+            mainController.getMainApplicationLayout().setCenter(newsPane);
+            sceneRefresher(actionEvent, root);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void weatherInterfaceLoader(ActionEvent actionEvent) {
+        FXMLLoader rootLoader = new FXMLLoader(WeatherReportApplication.class.getResource(interfaceLoader.MAIN.getInterface()));
+        FXMLLoader weatherLoader = new FXMLLoader(WeatherReportApplication.class.getResource(interfaceLoader.WEATHER_INTERFACE.getInterface()));
+        try {
+            Parent root = rootLoader.load();
+            weatherLoader.load();
+            MainController mainController = rootLoader.getController();
+            WeatherReportController reportController = weatherLoader.getController();
+            StackPane weatherStackPane = reportController.weatherStackPane;
+            mainController.getMainApplicationLayout().setCenter(weatherStackPane);
+            sceneRefresher(actionEvent, root);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void sceneRefresher(ActionEvent actionEvent, Parent root) {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root,700,460);
+        Scene scene = new Scene(root, 700, 460);
         stage.setScene(scene);
         stage.show();
     }
