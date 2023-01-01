2db3eb1940264657f873727b7cb688dcd5aa4263
Younes Halim <younes.halim@gmail.com>
2022-12-31 22:55:28 -0500

    Adding new modules

--- WeatherReport/src/main/java/module-info.java
+++ WeatherReport/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.weatherreport.weatherreport {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
@@ -9,15 +8,21 @@ module com.weatherreport.weatherreport {
     requires lombok;
     requires io.github.cdimascio.dotenv.java;
     requires com.google.gson;
+    requires java.desktop;
+    requires javafx.swing;
+    requires jakarta.mail;
+    requires jakarta.activation;
 
     opens com.weatherreport.weatherreport to javafx.fxml;
+    opens com.weatherreport.weatherreport.controllers to javafx.fxml;
+    opens com.weatherreport.weatherreport.model.meteorology to com.google.gson;
+    opens com.weatherreport.weatherreport.model.unsplash to com.google.gson;
+    opens com.weatherreport.weatherreport.model.location to com.google.gson;
+    opens com.weatherreport.weatherreport.model.Quotes to com.google.gson;
+
     exports com.weatherreport.weatherreport;
     exports com.weatherreport.weatherreport.controllers;
     exports com.weatherreport.weatherreport.model.location;
     exports com.weatherreport.weatherreport.model.Quotes;
-    opens com.weatherreport.weatherreport.controllers to javafx.fxml;
-    opens com.weatherreport.weatherreport.model.meteorology to com.google.gson;
-    opens com.weatherreport.weatherreport.model.news to com.google.gson;
-    opens com.weatherreport.weatherreport.model.location to com.google.gson;
-    opens com.weatherreport.weatherreport.model.Quotes to com.google.gson;
+    exports com.weatherreport.weatherreport.model.unsplash;
 }
