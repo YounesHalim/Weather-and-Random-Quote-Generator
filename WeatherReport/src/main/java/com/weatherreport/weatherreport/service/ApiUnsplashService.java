7a7799f713649c78bac646341dbac1ddc291c694
Younes Halim <younes.halim@gmail.com>
2022-12-31 22:52:37 -0500

    Unsplash service

---
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/service/ApiUnsplashService.java
@@ -0,0 +1,51 @@
+package com.weatherreport.weatherreport.service;
+
+import com.google.gson.Gson;
+import com.weatherreport.weatherreport.model.apicall.CallUnsplashAPI;
+import com.weatherreport.weatherreport.model.unsplash.Unsplash;
+import lombok.SneakyThrows;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ApiUnsplashService implements CallUnsplashAPI {
+    private static ApiUnsplashService apiUnsplashService;
+    public static List<String> listOfURLs = new ArrayList<>();
+    public ApiUnsplashService() {
+    }
+    public static synchronized ApiUnsplashService getApiUnsplashService() {
+        if(apiUnsplashService == null) {
+            apiUnsplashService = new ApiUnsplashService();
+        }
+        return apiUnsplashService;
+    }
+    @Override
+    public String getJSONAsAString() {
+        return CallUnsplashAPI.super.getJSONAsAString();
+    }
+    @SneakyThrows
+    @Override
+    public <T> T deserializedJsonObject() {
+        Gson gson = new Gson();
+        Callable<Unsplash> unsplashCallable = () -> gson.fromJson(getJSONAsAString(), Unsplash.class);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Unsplash> unsplashFuture = executorService.submit(unsplashCallable);
+
+        return (T) unsplashFuture.get();
+    }
+
+    public void setListOfURLs() {
+        Unsplash unsplashObject = deserializedJsonObject();
+        listOfURLs = unsplashObject
+                .getResults()
+                .parallelStream()
+                .map((results -> results.getUrls().getRegular()+".jpeg"))
+                .toList();
+    }
+    public static List<String> getListOfURLs() {
+        return listOfURLs;
+    }
+}
