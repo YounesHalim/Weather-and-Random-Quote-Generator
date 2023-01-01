f39e9400c2282955f20d36cb24791329463fdbfa
Younes Halim <younes.halim@gmail.com>
2022-12-31 23:15:25 -0500

    Minor fixes

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/SearchBarController.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/SearchBarController.java
@@ -1,35 +1,18 @@
 package com.weatherreport.weatherreport.controllers;
 
 import com.google.gson.Gson;
-import com.weatherreport.weatherreport.WeatherReportApplication;
 import com.weatherreport.weatherreport.model.location.GeographicLocation;
-import javafx.application.Platform;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.SnapshotParameters;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import org.controlsfx.control.textfield.TextFields;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
 import java.util.function.Function;
 
 public class SearchBarController{
-
     public static List<String> worldCitiesList = new ArrayList<>();
     public static void fetchJSONData() {
         String path = "src/main/resources/com/weatherreport/weatherreport/cities.json";
