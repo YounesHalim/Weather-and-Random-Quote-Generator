7bee1857ac6a6d42baddb172e59b77770af81b09
Younes Halim <younes.halim@gmail.com>
2023-01-03 14:27:02 -0500

    Minor update

--- WeatherReport/src/test/java/com/weatherreport/weatherreport/service/UnsplashServiceTest.java
+++ WeatherReport/src/test/java/com/weatherreport/weatherreport/service/UnsplashServiceTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 
 
 
-class ApiUnsplashServiceTest {
+class UnsplashServiceTest {
 
     @Test
     @DisplayName("Testing API CALL")
     void callAPI() {
-        Unsplash unsplash = ApiUnsplashService.getApiUnsplashService().deserializedJsonObject();
+        Unsplash unsplash = UnsplashService.getApiUnsplashService().deserializedJsonObject();
         System.out.println(unsplash.getTotal());
         System.out.println(unsplash.getTotal_pages());
         unsplash.getResults().forEach((element)-> System.out.println(element.getUrls().getFull()));
