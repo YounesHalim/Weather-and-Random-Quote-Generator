02acb5e1b0e7eb4459168eb6b46b8abf817fa7ab
Younes Halim <younes.halim@gmail.com>
2023-01-03 14:27:45 -0500

    Minor update

--- WeatherReport/src/test/java/com/weatherreport/weatherreport/service/ZenQuotesServiceTest.java
+++ WeatherReport/src/test/java/com/weatherreport/weatherreport/service/ZenQuotesServiceTest.java
@@ -1,19 +1,14 @@
 package com.weatherreport.weatherreport.service;
 
-import com.google.gson.JsonObject;
 import com.weatherreport.weatherreport.model.Quotes.Quote;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ApiZenQuotesServiceTest {
+class ZenQuotesServiceTest {
     @Test
     @DisplayName("API call test")
     void quoteCall() {
-        Quote[] quoteObject = ApiZenQuotesService.getQuotesInstance().deserializedGsonObject();
+        Quote[] quoteObject = ZenQuotesService.getQuotesInstance().deserializedGsonObject();
         System.out.println(quoteObject[0].getA());
         System.out.println(quoteObject[0].getQ());
     }
