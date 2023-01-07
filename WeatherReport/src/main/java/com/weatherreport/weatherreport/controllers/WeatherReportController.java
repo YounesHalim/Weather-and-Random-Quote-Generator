2a60b58c4d2d6f24f090dc7ada612062634164fd
Younes Halim <younes.halim@gmail.com>
2023-01-06 21:01:51 -0500

    Cleaning code

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/WeatherReportController.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/WeatherReportController.java
@@ -16,6 +16,7 @@ import javafx.scene.shape.Circle;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.net.MalformedURLException;
 import java.text.MessageFormat;
 import java.util.Arrays;
@@ -29,12 +30,17 @@ import static com.weatherreport.weatherreport.service.WeatherService.getWeatherS
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class WeatherReportController{
-    @FXML private AnchorPane weatherAnchorPane;
-    @FXML private TextField searchBar;
-    @FXML private Label lowTemp, highTemp, newsTitle, feelsLike, sunsetHour,sunriseHour, degree, countryLocation, weatherDescription, mainDescription;
-    @FXML private Label pressure, humidity, visibility, windData;
-    @FXML private Circle weatherIcon, sunsetIcon, sunriseIcon, visibilityIcon, humidityIcon, pressureIcon, windIcon, minTempIcon, maxTempIcon;
+public class WeatherReportController {
+    @FXML
+    private AnchorPane weatherAnchorPane;
+    @FXML
+    private TextField searchBar;
+    @FXML
+    private Label lowTemp, highTemp, newsTitle, feelsLike, sunsetHour, sunriseHour, degree, countryLocation, weatherDescription, mainDescription;
+    @FXML
+    private Label pressure, humidity, visibility, windData;
+    @FXML
+    private Circle weatherIcon, sunsetIcon, sunriseIcon, visibilityIcon, humidityIcon, pressureIcon, windIcon, minTempIcon, maxTempIcon;
     public static GeographicLocation defaultLocation = GeographicLocation.builder().country("CA").name("Montreal").lng("45.5019").lat("73.5674").build();
     private String celsius = "%d°C";
 
@@ -58,55 +64,56 @@ public class WeatherReportController{
         String icon = new Weather().getWeatherAttributes(forecast, Weather::getIcon);
         try {
             String iconPath = getWeatherServiceInstance().getAppropriateWeatherIcon(icon).toString();
-            weatherIcon.setFill(new ImagePattern(new Image(Objects.requireNonNull(iconPath))));
-            sunsetIcon.setFill(new ImagePattern(new Image(Objects.requireNonNull(WeatherReportApplication.class.getResourceAsStream("icons/sunset.png")))));
-            sunriseIcon.setFill(new ImagePattern(new Image(Objects.requireNonNull(WeatherReportApplication.class.getResourceAsStream("icons/sunrise.png")))));
-            humidityIcon.setFill(new ImagePattern(new Image(Objects.requireNonNull(WeatherReportApplication.class.getResourceAsStream("icons/humidity.png")))));
-            visibilityIcon.setFill(new ImagePattern(new Image(Objects.requireNonNull(WeatherReportApplication.class.getResourceAsStream("icons/visible.png")))));
-            windIcon.setFill(new ImagePattern(new Image(Objects.requireNonNull(WeatherReportApplication.class.getResourceAsStream("icons/wind.png")))));
+            weatherIcon.setFill(new ImagePattern(new Image(Objects.requireNonNull(iconPath), 32, 32, false, false)));
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
     }
+
     private void setHeliosTime(String firstOrLastLight, long sunriseSunsetTime) {
         Date convertedDate = new Date(sunriseSunsetTime * 1000);
         switch (firstOrLastLight) {
-            case "LAST" ->
-                    sunsetHour.setText(convertedDate.getHours() + ":" + convertedDate.getMinutes());
-            case "FIRST"->
-                    sunriseHour.setText(convertedDate.getHours() + ":" + convertedDate.getMinutes());
+            case "LAST" -> sunsetHour.setText(convertedDate.getHours() + ":" + convertedDate.getMinutes());
+            case "FIRST" -> sunriseHour.setText(convertedDate.getHours() + ":" + convertedDate.getMinutes());
         }
     }
+
     private void setCountryLocation(Meteorology forecast, Sys country) {
         String s = "%s, %s".formatted(forecast.getName(), country.getCountry());
         countryLocation.setText(s);
         visibility.setText(MessageFormat.format("{0}km", forecast.getVisibility()));
         windData.setText(MessageFormat.format("{0}km/h", (int) Math.floor(forecast.getWind().getSpeed())));
     }
+
     private void setFeelsLike(MainWeather mainWeather) {
         int feelsLikeTemp = (int) (Math.floor(mainWeather.getFeels_like()));
-        feelsLike.setText(String.format(celsius,feelsLikeTemp));
+        feelsLike.setText(String.format(celsius, feelsLikeTemp));
     }
+
     private void setTemperature(MainWeather mainWeather) {
-        degree.setText(String.format(celsius,(int) Math.floor(mainWeather.getTemp())));
+        degree.setText(String.format(celsius, (int) Math.floor(mainWeather.getTemp())));
         pressure.setText((MessageFormat.format("{0} hPa", mainWeather.getPressure())));
-        humidity.setText(MessageFormat.format("{0}%",mainWeather.getHumidity()));
-        lowTemp.setText(String.format(celsius,(int) Math.floor(mainWeather.getTemp_min())));
-        highTemp.setText(String.format(celsius,(int) Math.floor(mainWeather.getTemp_max())));
+        humidity.setText(MessageFormat.format("{0}%", mainWeather.getHumidity()));
+        lowTemp.setText(String.format(celsius, (int) Math.floor(mainWeather.getTemp_min())));
+        highTemp.setText(String.format(celsius, (int) Math.floor(mainWeather.getTemp_max())));
 
     }
+
     private void setDescription(String description) {
         // Capitalize
         mainDescription.setText(Arrays.stream(description.split(" "))
-                .map((word)-> word.toUpperCase().charAt(0) + word.substring(1))
+                .map((word) -> word.toUpperCase().charAt(0) + word.substring(1))
                 .collect(Collectors.joining(" ")));
     }
-    private void setMainDescription(String description)  {
+
+    private void setMainDescription(String description) {
         weatherDescription.setText(description);
     }
+
     protected Meteorology setWeatherApiCall(GeographicLocation news) {
         return getWeatherServiceInstance().weatherApiCall(news);
     }
+
     public static void setDefaultLocation(GeographicLocation defaultLocation) {
         WeatherReportController.defaultLocation = defaultLocation;
     }
@@ -117,7 +124,7 @@ public class WeatherReportController{
                 case ENTER -> {
                     if (!searchBar.getText().isEmpty() || !searchBar.getText().isBlank()) {
                         Function<TextField, String[]> nextDestination = (textField) -> textField.getText().split(",");
-                        GeographicLocation newLocation = new SearchBarController().searchedLocation(searchBar,nextDestination);
+                        GeographicLocation newLocation = new SearchBarController().searchedLocation(searchBar, nextDestination);
                         WeatherReportController.setDefaultLocation(newLocation);
                         initialize();
                     }
