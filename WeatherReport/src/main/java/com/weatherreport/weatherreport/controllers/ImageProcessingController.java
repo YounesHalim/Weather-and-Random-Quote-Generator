547f242a28f15337d3b51a549f5a6887f1f96de1
Younes Halim <younes.halim@gmail.com>
2023-01-18 23:02:00 -0500

    New class to handle image processing, grayscale, inverted filters

---
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/ImageProcessingController.java
@@ -0,0 +1,29 @@
+package com.weatherreport.weatherreport.controllers;
+
+
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.paint.Color;
+
+public class ImageProcessingController {
+    public enum Filters {
+        GRAYSCALE, SEPIA, BLACK_WHITE, INVERTED
+    }
+
+    protected void applyFilter(Filters filter, PixelWriter pixelWriter, PixelReader pixelReader, int i, int j) {
+        switch (filter) {
+            case GRAYSCALE -> grayscale(pixelWriter, pixelReader, i, j);
+        }
+    }
+    private void grayscale(PixelWriter pixelWriter, PixelReader pixelReader, int i, int j) {
+        Color color = pixelReader.getColor(j, i);
+        double avg = (color.getRed() + color.getGreen() + color.getBlue()) / 3;
+        color = new Color(avg, avg, avg, color.getOpacity());
+        pixelWriter.setColor(j, i, color);
+    }
+
+    private void invert(PixelWriter pixelWriter, PixelReader pixelReader, int i, int j) {
+        Color color = pixelReader.getColor(j, i);
+        color = new Color(1 - color.getRed(), 1 - color.getGreen(), 1 - color.getBlue(), color.getOpacity());
+    }
+}
