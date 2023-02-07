aad28f85e0e2c10f0fbe3a48ea6f9cab6e435b5f
Younes Halim <younes.halim@gmail.com>
2023-02-07 18:48:52 -0500

    API KEY is now hardcoded

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/service/WeatherService.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/service/WeatherService.java
@@ -4,8 +4,7 @@ import com.google.gson.Gson;
 import com.weatherreport.weatherreport.controllers.WeatherReportController;
 import com.weatherreport.weatherreport.model.apicall.ApiCall;
 import com.weatherreport.weatherreport.model.location.GeographicLocation;
-import com.weatherreport.weatherreport.model.meteorology.*;
-import io.github.cdimascio.dotenv.Dotenv;
+import com.weatherreport.weatherreport.model.meteorology.Meteorology;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,18 +12,17 @@ import java.util.concurrent.*;
 
 public class WeatherService implements ApiCall{
     private static WeatherService weatherService;
-    private WeatherService() {}
+    public WeatherService() {}
     public static WeatherService getWeatherServiceInstance() {
         if (weatherService == null) {
             weatherService = new WeatherService();
         }
         return weatherService;
     }
-
     public Meteorology getMeteorologyObject(GeographicLocation cityLocation) {
         Meteorology meteorology;
         Gson gson = new Gson();
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityLocation.getName() + "," + cityLocation.getCountry() + "&appid=" + Dotenv.load().get("APIKEY_OPEN_WEATHER") + "&units=" + cityLocation.getMeasureUnits();
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityLocation.getName() + "," + cityLocation.getCountry() + "&appid=" + "6607b5ff364b393169198f44f10e6715" + "&units=" + cityLocation.getMeasureUnits();
         try {
             Callable<Meteorology> meteorologyCallable = () -> gson.fromJson(serializedJSONObject(url), Meteorology.class);
             ExecutorService executorService = Executors.newSingleThreadExecutor();
@@ -37,7 +35,7 @@ public class WeatherService implements ApiCall{
         return meteorology;
     }
     public URL getAppropriateWeatherIcon(String fetchedWeatherConditionIcon) throws MalformedURLException {
-        return new URL("http://openweathermap.org/img/wn/%s@2x.png".formatted(fetchedWeatherConditionIcon));
+        return new URL("https://openweathermap.org/img/wn/%s@2x.png".formatted(fetchedWeatherConditionIcon));
     }
     public Meteorology weatherApiCall(GeographicLocation location) {
         GeographicLocation news = GeographicLocation
@@ -51,12 +49,10 @@ public class WeatherService implements ApiCall{
                 .getWeatherServiceInstance()
                 .getMeteorologyObject(news);
     }
-
     @Override
     public <T> T serializedJSONObject(T url) {
         return ApiCall.super.serializedJSONObject(url);
     }
-
     @Override
     public <T> T deserializedJSONObject() {
         return (T) getMeteorologyObject(WeatherReportController.defaultLocation);
