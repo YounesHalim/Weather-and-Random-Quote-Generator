44b1c822f4d1755eb25f0629ac19934f6fa47dd4
Younes Halim <younes.halim@gmail.com>
2023-02-07 17:59:34 -0500

    Buttons are now replaced with circle shaped objected simulating the same function.

--- WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/MainController.java
+++ WeatherReport/src/main/java/com/weatherreport/weatherreport/controllers/MainController.java
@@ -6,56 +6,51 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
-import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MainController implements Initializable {
-    @FXML
-    private BorderPane mainApplicationLayout;
-    @FXML
-    private Button weather, quotes;
+    @FXML private BorderPane mainApplicationLayout;
+    @FXML private VBox vboxOptionMenu;
+    @FXML private Circle weather, quotes;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        weather.setCursor(Cursor.HAND);
-        quotes.setCursor(Cursor.HAND);
-        weather.setOnAction(actionEvent ->
-                new Thread(()-> setInterface(interfaceLoader.WEATHER_INTERFACE.getInterface())).start());
-        quotes.setOnAction(actionEvent ->
-                new Thread(()-> setInterface(interfaceLoader.QUOTE_INTERFACE.getInterface())).start());
+        setIcons();
+        cursorHandler();
+        weather.setOnMouseClicked(actionEvent -> setInterface(interfaceLoader.WEATHER_INTERFACE.getInterface()));
+        quotes.setOnMouseClicked(actionEvent -> setInterface(interfaceLoader.QUOTE_INTERFACE.getInterface()));
     }
-
     public enum interfaceLoader {
-        SEARCH_BAR_INTERFACE("fxml/seachScene.fxml"),
-        WEATHER_INTERFACE("fxml/weatherScene.fxml"),
-        NEWS_INTERFACE("newsScene.fxml"),
         MENU_NAV("fxml/menuNavigator.fxml"),
         QUOTE_INTERFACE("fxml/QuotesEditor.fxml"),
         SHARE_INTERFACE("fxml/share.fxml"),
-        MAIN("MainScene.fxml");
-        private String fxmlFile;
+        WEATHER_INTERFACE("fxml/weatherScene.fxml");
+        private final String fxmlFile;
 
         interfaceLoader(String fxmlFile) {
             this.fxmlFile = fxmlFile;
         }
-
         public String getInterface() {
             return fxmlFile;
         }
     }
-
     private void setInterface(String scene) {
-        Platform.runLater(()-> {
+        Platform.runLater(() -> {
             try {
                 FXMLLoader loader = new FXMLLoader();
                 loader.setLocation(WeatherReportApplication.class.getResource(scene));
@@ -65,5 +60,13 @@ public class MainController implements Initializable {
                 throw new RuntimeException(e);
             }
         });
+    }
+    private void cursorHandler() {
+        vboxOptionMenu.getChildren().forEach(node -> node.setCursor(Cursor.HAND));
+    }
+
+    private void setIcons() {
+        weather.setFill(new ImagePattern(new Image(Objects.requireNonNull(WeatherReportApplication.class.getResourceAsStream("icons/weather.png")))));
+        quotes.setFill(new ImagePattern(new Image(Objects.requireNonNull(WeatherReportApplication.class.getResourceAsStream("icons/portrait2.jpeg")))));
     }
 }
